b887f5d4cad5e1c16d7215eb8b1dc02480750a12
Joseph Fahey <joseph@bigdog.eerayell.net>
2008-03-23 22:42:11 +0100

    Just starting out with indexing class.

---
+++ php/folksoIndexCache.php
@@ -0,0 +1,22 @@
+<?php
+
+class folksoIndexCache {
+
+  var $cachedir;
+  var $cache_prefix = 'folksoindex-';
+  var $cache_suffix = '.cache';
+  var $cache_file_limit;
+
+  function folksoIndexCache ($dir, $cache_prefix, $cache_suffix, $file_limit) {
+    $this->cachedir = $dir;
+    $this->cache_prefix = $cache_prefix;;
+    $this->cache_suffix = $cache_suffix;
+    $this->cache_file_limit = $cache_file_limit ? $cache_file_limit : 100;
+      
+
+  }
+
+
+
+
+?>
