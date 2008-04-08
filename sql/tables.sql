5c020e987fc69ebbfe151f3ced21cd86fded367e
Joseph Fahey <joseph@bigdog.eerayell.net>
2008-04-08 22:10:42 +0200

    updating

--- sql/tables.sql
+++ sql/tables.sql
@@ -5,7 +5,7 @@ drop table if exists resource;
 create table resource
        (id int unsigned primary key auto_increment,
         uri_normal varchar(255) not null unique,
-        uri_raw smalltext not null,
-        title null,
-        visited int default 1 not null);
+        uri_raw mediumtext not null,
+        title mediumtext null,
+        visited int unsigned default 1 not null);
 
