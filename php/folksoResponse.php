999d51ddc25dd307e903705f9b4bc16ac7ce9cee
Joseph Fahey <joseph@bigdog.eerayell.net>
2008-04-21 23:48:53 +0200

    Added some documentation to folksoResponse.

--- php/folksoResponse.php
+++ php/folksoResponse.php
@@ -1,4 +1,22 @@
 <?php
+
+  /**
+   * folksoResponse provides objects to be used as args to the
+   * addResponseObj method in folksoServer.php. Each object must
+   * contain a test function and an "action" function. The test
+   * function is called to determine if the action function should be
+   * called or not.
+   *
+   * Both test and action functions receive folksoQuery objects as
+   * mandatory arguments. Test functions must return either true or
+   * false. Action functions should provide a document of some sort to
+   * return and probably should provide a status code.
+   *
+   * @package Folkso
+   * @author Joseph Fahey
+   * @copyright 2008 Gnu Public Licence (GPL)
+   */
+
  
 class folksoResponse {
   public $httpMethod = 'GET';
