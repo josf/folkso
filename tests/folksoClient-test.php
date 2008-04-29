72f0c2675e9818a67595798ba279c1abfb1d9069
Joseph Fahey <joseph@bigdog.eerayell.net>
2008-04-29 23:00:27 +0200

    Added POST tests to folksoClient-test.

--- tests/folksoClient-test.php
+++ tests/folksoClient-test.php
@@ -6,6 +6,7 @@ include('/usr/local/www/apache22/lib/jf/fk/folksoClient.php');
 
 class testOffolksoClient extends  UnitTestCase {
   public $cl;
+  public $pos;
 
 
   function testConstruction () {
@@ -28,11 +29,25 @@ class testOffolksoClient extends  UnitTestCase {
     $this->assertEqual($this->cl->content_length, 0);
   }
 
+
+  function testPost () {
+    $fields = array('folksoStuff' => 'bob',
+                    'folksoThing' => 'blender');
+    $this->pos = new folksoClient('localhost', '/tag.php', 'POST');
+    $this->pos->set_postfields($fields);
+    $this->assertTrue(is_string($this->pos->postfields));
+    $this->assertEqual($this->pos->postfields, 'folksoStuff=bob&folksoThing=blender');
+    $this->assertEqual($this->pos->build_req(), 'localhost/tag.php');
+    $this->assertEqual($this->pos->content_length(), 35);
+  }
+
   function testRequest () {
     $this->cl->execute();
     $this->assertEqual(curl_getinfo($this->cl->ch, CURLINFO_HTTP_CODE), 200);
-
+    $this->pos->execute();
+    $this->assertEqual(curl_getinfo($this->pos->ch, CURLINFO_HTTP_CODE), 200);
   }
+
 }
 
 
