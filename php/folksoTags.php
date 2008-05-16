66e36eacbcef579209631381b699c69d5a537db8
Joseph Fahey <joseph@bigdog.eerayell.net>
2008-05-16 22:29:42 +0200

    commiting just to be able to pull from fab.

--- php/folksoTags.php
+++ php/folksoTags.php
@@ -6,8 +6,8 @@ require_once('/usr/local/www/apache22/lib/jf/fk/folksoUrl.php');
 require_once('/usr/local/www/apache22/lib/jf/fk/folksoServer.php');
 require_once('/usr/local/www/apache22/lib/jf/fk/folksoResponse.php');
 require_once('/usr/local/www/apache22/lib/jf/fk/folksoQuery.php');
-require_once('/usr/local/www/apache22/lib/jf/fk/folksoUserCreds.php');
+//require_once('/usr/local/www/apache22/lib/jf/fk/folksoUserCreds.php');
 require_once('/usr/local/www/apache22/lib/jf/fk/folksoDBconnect.php');
 require_once('/usr/local/www/apache22/lib/jf/fk/folksoDataDisplay.php');
-//require_once('/usr/local/www/apache22/lib/jf/fk/folksoWsseCreds.php');
+require_once('/usr/local/www/apache22/lib/jf/fk/folksoWsseCreds.php');
 ?>
