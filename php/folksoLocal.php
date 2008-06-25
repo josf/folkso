f1f7fd6f66f8dc0b76896144aa528aa64b446be1
Joseph Fahey <joseph@feather.eerayell.net>
2008-06-25 16:45:25 +0200

    just checking (in). fkLocal in very simple state still.

--- php/folksoLocal.php
+++ php/folksoLocal.php
@@ -20,12 +20,31 @@ abstract class folksoLocal {
    * '/folkso/tag.php'. 
    *
    * Must not include the hostname.
+   *
    */
-  public $server_web_path;
-  
+  protected $server_web_path;
 
-
-
+  /**
+   * Simple setter function. 
+   * 
+   * @params string $path : the path part of the uri for your
+   * tagserver.
+   *
+   * A trailing '/' will be added if not already present.
+   */  
+  public function set_server_web_path ($path) {
+    if (substr($path, -1) != '/') {
+      $path = $path . '/';
+    }
+    $this->server_web_path = $path;
   }
 
+  /** 
+   * Very simple getter function.
+   */
+  public function get_server_web_path () {
+    return $this->server_web_path;
+  }
+
+  }
 ?>
