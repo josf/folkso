8a685fed187b6bc3ca43768b0dab3658044eea9b
Joseph Fahey <fahey.joseph@gmail.com>
2008-08-15 16:24:20 +0200

    Metatag boxes: produced in taglists, autocompletion working. Have not yet checed on what happens when you click on "Valider".

--- js/resedit.js
+++ js/resedit.js
@@ -1,5 +1,5 @@
-var urlbase = "http://www.fabula.org/commun3/folksonomie/";
-//var urlbase = "http://localhost/";
+//var urlbase = "http://www.fabula.org/commun3/folksonomie/";
+var urlbase = "http://localhost/";
 
 $(document).ready(function() {
   $("#showsql").click(
@@ -26,157 +26,158 @@ $(document).ready(function() {
 
   $("ul.editresources li").each(iframePrepare);
   $("ul.editresources li").each(tagboxPrepare);
-  $("ul.editresources li").each(taglistHidePrepare);
-  $("a.closeiframe").hide();
-  $("#grouptagvalidate").click(
-    function(event) {
-      event.preventDefault();
-    $("input.groupmod:checked").each(groupTag);
-    });
-  $("#grouptagbox").autocomplete(urlbase + "tagcomplete.php");
-//  $("ul.taglist li").each(tagremovePrepare);
-});
+   $("ul.editresources li").each(taglistHidePrepare);
+   $("a.closeiframe").hide();
+   $("#grouptagvalidate").click(
+     function(event) {
+       event.preventDefault();
+     $("input.groupmod:checked").each(groupTag);
+     });
+   $("#grouptagbox").autocomplete(urlbase + "tagcomplete.php");
+ //  $("ul.taglist li").each(tagremovePrepare);
+ });
 
-/**
- * "tag" can be either a tagnorm or an id. Returns a correct tag url.
- */
-function tagUrl(tag) {
-  return "http://localhost/tagview.php?tag=" + tag; // this is probably wrong!
-}
+ /**
+  * "tag" can be either a tagnorm or an id. Returns a correct tag url.
+  */
+ function tagUrl(tag) {
+   return "http://localhost/tagview.php?tag=" + tag; // this is probably wrong!
+ }
 
-/**
- *  "this" must be a list element from the main list of resources.
- */
-function iframePrepare() {
-  var url = $(this).find("a.resurl").attr("href");
-  var holder = $(this).find("div.iframeholder");
-  $(this).find("a.openiframe").click(
-    function(event) {
-      event.preventDefault();
-      var ifr = document.createElement("iframe");
-      ifr.src = url;
-      ifr.className = "preview";
+ /**
+  *  "this" must be a list element from the main list of resources.
+  */
+ function iframePrepare() {
+   var url = $(this).find("a.resurl").attr("href");
+   var holder = $(this).find("div.iframeholder");
+   $(this).find("a.openiframe").click(
+     function(event) {
+       event.preventDefault();
+       var ifr = document.createElement("iframe");
+       ifr.src = url;
+       ifr.className = "preview";
 
-      if (holder.children().is("iframe")) {
-        holder.show();
-      }
-      else {
-        holder.append(ifr);
-      }
-      $(this).hide();
-      $(this).parent().find("a.closeiframe").show();
-    }
-  );
-  $(this).find("a.closeiframe").click(
-    function(event) {
-      event.preventDefault();
-      holder.hide();
-      $(this).hide();
-      $(this).parent().find("a.openiframe").show();
-    }
-  );
-}
+       if (holder.children().is("iframe")) {
+         holder.show();
+       }
+       else {
+         holder.append(ifr);
+       }
+       $(this).hide();
+       $(this).parent().find("a.closeiframe").show();
+     }
+   );
+   $(this).find("a.closeiframe").click(
+     function(event) {
+       event.preventDefault();
+       holder.hide();
+       $(this).hide();
+       $(this).parent().find("a.openiframe").show();
+     }
+   );
+ }
 
-  /**
-   * To be called on a <li>. Sets up tagging input box with
-   * autocompletion and refreshes the tagmenu.
-   */
-function tagboxPrepare() {
-    var lis = $(this);
-    var tgbx = lis.find("input.tagbox");
-    tgbx.autocomplete(urlbase + "tagcomplete.php");
+   /**
+    * To be called on a <li>. Sets up tagging input box with
+    * autocompletion and refreshes the tagmenu.
+    */
+ function tagboxPrepare() {
+     var lis = $(this);
+     var tgbx = lis.find("input.tagbox");
+     tgbx.autocomplete(urlbase + "tagcomplete.php");
 
-    var url = lis.find("a.resurl").attr("href");
-    lis.find("a.tagbutton").click(
-        function(event) {
-            event.preventDefault();
-            if (tgbx.val()) {
-                 $.ajax({
-                   url: urlbase + 'resource.php',
-                   type: 'post',
-                   datatype: 'text/text',
+     var url = lis.find("a.resurl").attr("href");
+     lis.find("a.tagbutton").click(
+         function(event) {
+             event.preventDefault();
+             if (tgbx.val()) {
+                  $.ajax({
+                    url: urlbase + 'resource.php',
+                    type: 'post',
+                    datatype: 'text/text',
                    data: {
-                     folksores: url,
-                     folksotag: tgbx.val()},
-                   error: function(xhr, msg) {
-                       alert("Autocomplete request failed: " +
-                              xhr.statusText + " " + xhr.responseText);
-                     },
-                   success: function (str) {
-                     getTagMenu(
-                       lis.find("div.emptytags"),
-                       lis.attr("id").substring(3));
-                     tgbx.val('');
-                   }
-                 });
-            }
-          else {
-          alert('Il faut choisir un tag d\'abord');
-        }
-    });
-}
+                      folksores: url,
+                      folksotag: tgbx.val()},
+                    error: function(xhr, msg) {
+                        alert("Autocomplete request failed: " +
+                               xhr.statusText + " " + xhr.responseText);
+                      },
+                    success: function (str) {
+                      getTagMenu(
+                        lis.find("div.emptytags"),
+                        lis.attr("id").substring(3));
+                      tgbx.val('');
+                    }
+                  });
+             }
+           else {
+           alert('Il faut choisir un tag d\'abord');
+         }
+     });
+ }
 
-/**
- * To be called on a ul.tagmenu
+
+ /**
+  * To be called on a ul.tagmenu
+  */
+ function tagremovePrepare() {
+   var remove = $(this).find("a.remtag");
+
+   var taglistdiv = $(this).parent();
+   var resourceid = taglistdiv.parent().parent().attr("id").substring(3);
+
+   remove.click(function(event) {
+         event.preventDefault();
+         var tagid = $(this).siblings(".tagid").text();
+         $.ajax({
+             url: urlbase + 'resource.php',
+             type: 'post',
+             data: {
+               folksores: resourceid,
+               folksotag: tagid,
+               folksodelete: 1
+             },
+             error: function(xhr, msg) {
+               alert(msg);
+             },
+             success: function(data) {
+               getTagMenu(
+                 taglistdiv,
+                 resourceid
+                 );
+             }
+             });
+         });
+ }
+ /**
+ * To be called on a list element.
  */
-function tagremovePrepare() {
-  var remove = $(this).find("a.remtag");
+ function taglistHidePrepare() {
+   var resourceid = $(this).attr("id").substring(3);
+   var lis = $(this);
 
-  var taglistdiv = $(this).parent();
-  var resourceid = taglistdiv.parent().parent().attr("id").substring(3);
+   $(this).find("a.seetags").click(
+     function(event) {
+       event.preventDefault();
+       getTagMenu(lis.find("div.emptytags"), resourceid);
+     }
+   );
 
-  remove.click(function(event) {
-        event.preventDefault();
-        var tagid = $(this).siblings(".tagid").text();
-        $.ajax({
-            url: urlbase + 'resource.php',
-            type: 'post',
-            data: {
-              folksores: resourceid,
-              folksotag: tagid,
-              folksodelete: 1
-            },
-            error: function(xhr, msg) {
-              alert(msg);
-            },
-            success: function(data) {
-              getTagMenu(
-                taglistdiv,
-                resourceid
-                );
-            }
-            });
-        });
-}
-/**
-* To be called on a list element.
-*/
-function taglistHidePrepare() {
-  var resourceid = $(this).attr("id").substring(3);
-  var lis = $(this);
+   $(this).find("a.hidetags").click(
+     function(event) {
+       event.preventDefault();
+       lis.find("ul.tagmenu").remove();
+       }
+   );
+ }
 
-  $(this).find("a.seetags").click(
-    function(event) {
-      event.preventDefault();
-      getTagMenu(lis.find("div.emptytags"), resourceid);
-    }
-  );
-
-  $(this).find("a.hidetags").click(
-    function(event) {
-      event.preventDefault();
-      lis.find("ul.tagmenu").remove();
-      }
-  );
-}
-
-/**
- * Create or update a tag menu. (Most of the work is done
- * by tagMenuFromXml.)
- */
-function getTagMenu(place, resid) {
-    place.find("ul.tagmenu").remove();
-    var tagMenuFromXmlFunction = tagMenuFunkMaker(place);
+ /**
+  * Create or update a tag menu. (Most of the work is done
+  * by tagMenuFromXml.)
+  */
+ function getTagMenu(place, resid) {
+     place.find("ul.tagmenu").remove();
+     var tagMenuFromXmlFunction = tagMenuFunkMaker(place, resid);
     $.ajax({ url: urlbase + 'resource.php',
            type: 'get',
            datatype: 'text/xml',
@@ -195,7 +196,7 @@ function getTagMenu(place, resid) {
  *
  *  Had to use this fancy closure system to get this to work.
  */
-function tagMenuFunkMaker(place) {
+function tagMenuFunkMaker(place, resid) {
   var dest = place;
   return function(xml) {
     var ul = $('<ul class="tagmenu">');
@@ -220,12 +221,57 @@ function tagMenuFunkMaker(place) {
                       + "</span>"));
 
         item.append($('<a class="remtag" href="#">Désassocier</a>'));
+
+        item.append(makeMetatagBox(resid, //closure
+                                   $(this).find('numid').text(),
+                                   place));
         ul.append(item);
       });
     dest.append(ul);
     $("ul.tagmenu").each(tagremovePrepare);
   };
 }
+
+
+/**
+ *  Parameter lis is a list item so that we know where to plug the
+ *  new taglist back in when we are done.
+ *
+ * resource and tag can be either text or ids.
+ */
+function makeMetatagBox (resource, tag, lis) {
+  var container = $("<span class='metatagbox'></span>")
+    .append("<span class='infohead'>Modifier le metatag </class>");
+
+  var box = $("<input type='text' class='metatagbox'>");
+  box.autocomplete(metatag_autocomplete_list); //variable defined in <script> on page.
+
+  var button = $("<a href='#'>metaValider</a>")
+    .click(
+      function(event){
+        event.preventDefault();
+        $.ajax({
+                 url: urlbase + 'resource.php',
+                 type: 'post',
+                 data: {
+                   folksores: resource,
+                   folksotag: tag,
+                   folksometa: this.val()
+                 },
+                 error: function(xhr, msg) {
+                   alert(msg);
+                 },
+                 success: function(data) {
+                   getTagMendu(lis, resource);
+                 }
+               });
+      });
+
+
+   container.append(box);
+   return container.append(button);
+}
+
 
 function groupTag() {
   var lis = $(this).parent().parent("ul.editresources li");
