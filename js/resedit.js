84645e0fa308b54c767c0c20fea9781067988d5b
Joseph Fahey <josf@fabula.org>
2008-08-16 23:06:41 +0200

    Using relative url now for ajax to fix the 'www' same server bug.

--- js/resedit.js
+++ js/resedit.js
@@ -1,5 +1,5 @@
-//var urlbase = "http://www.fabula.org/commun3/folksonomie/";
-var urlbase = "http://localhost/";
+var urlbase = "/commun3/folksonomie/";
+//var urlbase = "/";
 
 $(document).ready(
   function() {
