02e9f9a95f8ef3c6061210e1fbdedf5e1c816815
Joseph Fahey <fahey.joseph@gmail.com>
2008-08-19 00:26:54 +0200

    File to contain functions specific to tagedit.php.

---
+++ js/tagedit.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+                    $('.tagentry').each(fkPrepare);
+                  });
+
+
+function fkPrepare(selector) {
+  // tagid is 'tagid' + number
+  var thisid = $(this).attr('id');
+  var tagid = thisid.substring(5);
+
+  $(this).find("button.delete").click(
+    function(event) {
+      event.preventDefault();
+      $.post(urlbase + 'tag.php',
+             {folksotag: tagid,
+              folksodelete: ''},
+             function() {
+               $("#" + thisid).hide("slow");
+               $("#" + thisid).remove();
+             });
+    });
+  modifyTagPrep($(this));
+  mergeTagPrep($(this));
+}
