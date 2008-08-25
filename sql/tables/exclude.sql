744700a9b8e3c6393c3f7431d43f449561936515
Joseph Fahey <fahey.joseph@gmail.com>
2008-08-25 15:34:31 +0200

    Capitalized some sql.

--- sql/tables/exclude.sql
+++ sql/tables/exclude.sql
@@ -1,7 +1,7 @@
 -- each newly indexed resource should be checked against this table
 -- and rejected if found here.
 
-drop table if exists exclude;
-create table exclude
-       (uri_normal varchar(255) primary key,
-       exclusion_date timestamp default current_timestamp);
+DROP TABLE IF EXISTS exclude;
+CREATE TABLE exclude
+       (uri_normal VARCHAR(255) PRIMARY KEY,
+       exclusion_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
