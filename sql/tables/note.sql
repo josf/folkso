ac544ba2de1ac94362518fa8a68c24b2f53d945c
Joseph Fahey <fahey.joseph@gmail.com>
2008-08-27 17:02:11 +0200

    Small fixes to note.

--- sql/tables/note.sql
+++ sql/tables/note.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS note;
 CREATE TABLE note
        (id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-       resource_id INT UNSIGNED NOT NULL
-       text TEXT NOT NULL,
+       resource_id INT UNSIGNED NOT NULL,
+       note TEXT NOT NULL,
        user_id INT UNSIGNED NOT NULL DEFAULT 9999,
        INDEX resid (resource_id))
        ENGINE = InnoDB;
