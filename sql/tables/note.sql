4f8e732173df8a685b70594478cb9a54ba962a84
Joseph Fahey <fahey.joseph@gmail.com>
2008-08-27 15:29:37 +0200

    Just starting work on the annotation system. First draft.

---
+++ sql/tables/note.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS note;
+CREATE TABLE note
+       (id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+       resource_id INT UNSIGNED NOT NULL
+       text TEXT NOT NULL,
+       user_id INT UNSIGNED NOT NULL DEFAULT 9999,
+       INDEX resid (resource_id))
+       ENGINE = InnoDB;
+       
