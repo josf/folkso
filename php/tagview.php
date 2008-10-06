e0fab8d90ed5fefd1ef9ab58afb65340615299aa
Joseph Fahey <fahey.joseph@gmail.com>
2008-10-06 22:28:30 +0200

    Basic html + css. Beginning of conditional formatting.

--- php/tagview.php
+++ php/tagview.php
@@ -1,3 +1,17 @@
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+
+   <link 
+        rel="stylesheet" type="text/css" 
+        href="http://www.fabula.org/commun3/template.css" 
+        media="screen">
+    </link>
+
+<style type="text/css">
+#container { background-color: white; padding: 2em; }
+
+</style>
 <?php
 
 require_once('folksoPage.php');
@@ -6,16 +20,32 @@ $page = new folksoPage();
 
 $tagreq = $_GET['tag'];
 
-print $tagreq;
-
-print strlen($tagreq);
-
 if ((strlen($tagreq == 0)) ||
     (strlen($tagreq) > 300)) {
   die("Requête malformée, tag impossible");
 }
 
-$page->public_tag_resource_list($tagreq);
+$taglist = $page->public_tag_resource_list($tagreq);
 
+print "<title>Fabula - Tags - ";
+if (($taglist['status'] == 200) ||
+    ($taglist['status'] == 304)) {
+  print $tagreq;
+}
+else {
+  print " Erreur ";
+}
+
+print "</title>\n</head>\n<body>\n<div id='container'>";
+
+if (($taglist['status'] == 200) ||
+    ($taglist['status'] == 304)) {
+
+  print "<h1>Ressources associées au tag : </h1>\n";
+
+  print $taglist['html'];
+}
+
+print "</div>\n</body>\n";
 
 ?>
