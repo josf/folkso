e42a0e652b5d6413e3f5332f9c9d0f904e434d6f
Joseph Fahey <joseph@feather.eerayell.net>
2008-11-07 17:47:49 +0100

    Initial commit. Just getting started on rewrite.

---
+++ php/folksoPageDataMeta.php
@@ -0,0 +1,23 @@
+<?php
+
+class folksoPageDataMeta extends folksoPageData {
+
+
+  /**
+   * An array of the meta data tags.
+   */
+  public $array;
+
+  public function meta_keywords() {
+    return ""
+
+  }
+
+  }
+
+
+
+
+
+
+?>
