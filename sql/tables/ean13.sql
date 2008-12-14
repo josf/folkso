7598346ce5b4946eede118af14c046966a7df2d5
Joseph Fahey <fahey.joseph@gmail.com>
2008-12-14 11:24:56 +0100

    Unique index constraints for ean13.

--- sql/tables/ean13.sql
+++ sql/tables/ean13.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS ean13;
 CREATE TABLE ean13
        (resource_id INT UNSIGNED NOT NULL,
        ean13 BIGINT UNSIGNED NOT NULL,
-       INDEX resean (resource_id, ean13),
-       INDEX eanres (ean13, resource_id),
+       UNIQUE INDEX resean (resource_id, ean13),
+       UNIQUE INDEX eanres (ean13, resource_id),
        FOREIGN KEY (resource_id) REFERENCES resource (id))
 ENGINE=InnoDB;
