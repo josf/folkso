f3abc3b8704562b7c866645b683880a8ed942eb2
Joseph Fahey <fahey.joseph@gmail.com>
2008-12-22 14:15:25 +0100

    Just does cloud_by_popularity right now. Works (1 test).

---
+++ tests/folksoResQuery-test.php
@@ -0,0 +1,45 @@
+<?php
+require_once('unit_tester.php');
+require_once('reporter.php');
+
+require_once('folksoResQuery.php');
+
+class testOffolksoResQuery extends UnitTestCase {
+
+  function testBasic ()  {
+    $rq = new folksoResQuery();
+    $this->assertIsA($rq, folksoResQuery);
+    print $rq->cloud_by_popularity(4354, 3);
+
+  }
+}
+
+
+
+$test = &new testOffolksoResQuery();
+$test->run(new HtmlReporter());
+
+
+/**
+
+assertTrue($x) 	Fail if $x is false
+assertFalse($x) 	Fail if $x is true
+assertNull($x) 	Fail if $x is set
+assertNotNull($x) 	Fail if $x not set
+assertIsA($x, $t) 	Fail if $x is not the class or type $t
+assertNotA($x, $t) 	Fail if $x is of the class or type $t
+assertEqual($x, $y) 	Fail if $x == $y is false
+assertNotEqual($x, $y) 	Fail if $x == $y is true
+assertWithinMargin($x, $y, $m) 	Fail if abs($x - $y) < $m is false
+assertOutsideMargin($x, $y, $m) 	Fail if abs($x - $y) < $m is true
+assertIdentical($x, $y) 	Fail if $x == $y is false or a type mismatch
+assertNotIdentical($x, $y) 	Fail if $x == $y is true and types match
+assertReference($x, $y) 	Fail unless $x and $y are the same variable
+assertClone($x, $y) 	Fail unless $x and $y are identical copies
+assertPattern($p, $x) 	Fail unless the regex $p matches $x
+assertNoPattern($p, $x) 	Fail if the regex $p matches $x
+expectError($x) 	Swallows any upcoming matching error
+assert($e) 	Fail on failed expectation.html object $e
+
+ **/
+
