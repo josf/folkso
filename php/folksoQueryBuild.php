95bbb5a8cebb57f95e5349345ff712464621519b
Joseph Fahey <fahey.joseph@gmail.com>
2009-01-01 22:57:34 +0100

    Finally wrote the documentation for the QueryBuild DSL (so to speak).

--- php/folksoQueryBuild.php
+++ php/folksoQueryBuild.php
@@ -1,5 +1,68 @@
 <?php
+ /**
+   *
+   * @package Folkso
+   * @subpackage Tagserv
+   * @author Joseph Fahey
+   * @copyright 2008 Gnu Public Licence (GPL)
+   */
 
+  /**
+   * @package Folkso
+   *
+   * Build SQL queries based on the options given.
+   *
+   * An attempt at something like a DSL for writing SQL queries.
+   *
+   * The primary argument, $sql_arr, is an indexed array of
+   * associative arrays (array of hashes in Perl). Each assoc array
+   * has this form:
+   *
+   * array('type' => ...
+   *       'sql' => 'SELECT ... etc. ')
+   *
+   * The 'type' field determines whether or not the text in the 'sql'
+   * field will be included or not in the query that is returned. If
+   * 'type' is 'common', then the string in 'sql' will always be
+   * included.
+   *
+   * The second argument, $thing, is the resource or tag identifier or
+   * name (url for resources, tag string for tags).  Since this data
+   * can either be a number or a string, we can include specific SQL
+   * strings, using either 'isnum' or 'notnum' as the 'type' value.
+   *
+   * array('type' => 'isnum', 'sql' => 'where id = <<<x>>>');
+   *
+   * The value of $thing is inserted into the returned SQL query text
+   * every time the sequence <<<x>>> is found in the SQL strings.
+   *
+   * The $thing argument is common to all of these queries, but often
+   * other more diverse parameters are needed as well. These must be
+   * included in the third argument to the 'build' method,
+   * $arg_arr. 
+   * 
+   * In this case, we define "variables" that can have arbitrary names
+   * (except for 'x'). For a "variable" named 'limit', we would write
+   * <<<limit>>> in the SQL. These "variables" can be used both as
+   * variables within the SQL itself, and to determine whether a given
+   * SQL string will be included or not.
+   *
+   * $arg_arr is an assoc array of assoc arrays (HoH) where the keys
+   * in the primary array are the names of the "variables" used in the
+   * SQL strings. The value corresponding to each key is another assoc
+   * array containing two keys, 'func' and 'value'. 'func' can be a
+   * function that must return a boolean and is called to determine
+   * whether or not a given sequence is to be included. 'value' is the
+   * value that will be inserted in the place of <<<variable_name>>>,
+   * whatever 'variable_name' happens to be. 
+   *
+   * If the value of 'func' is undefined or false, no function is
+   * called to determine whether or not an SQL sequence should be
+   * included. If 'value' evaluates to TRUE, the sequence is included,
+   * and otherwise it is not. 
+   *
+   * 
+   */
 class folksoQueryBuild {
 
   public $sql;
