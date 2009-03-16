644756e55743dfccda0d0ff888051bc7e9157e6f
Joseph Fahey <fahey.joseph@gmail.com>
2009-03-16 13:59:29 +0100

    resupdate only partially written.

--- php/resupdate.php
+++ php/resupdate.php
@@ -28,8 +28,54 @@ $srv->addResponseObj(new folksoResponse('post',
 $srv->Respond();
 
 function reload (folksoQuery $q, folksoWsseCreds $cred, folksoDBconnect $dbc) {
+  $i = new folksoDBinteract($dbc);
+  if ($i->db_error()) {
+    header('HTTP/1.0 501 Database connection error');
+    die($i->error_info());
+  }
 
+  $url = '';
+  if (is_numeric($q->res)){
+    $url = $i->url_from_id($q->res);
+    if ($url = 0){ // no corresponding url
+      header('HTTP/1.1 404 Resource not found.');
+      print 
+        "The numeric id " . $q->res . " that was provided does not correspond "
+        ." to an existing  resource. Perhaps the resource has been deleted.";
+      return;
+    }
+  }
+  else{
+    $url = $q->res;
+    if (! $i->resourcep($url)) {
+      header('HTTP/1.1 404 Resource not found');
+      print 
+        "The url provided (" . $q->res . ") was not found in the database. "
+        . "It must be added before it can be modified.";
+      return;
+    }
+  }
 
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_USERAGENT, 'folksoClient');
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+  $result = curl_exec($ch);
+  $result_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+  curl_close($ch);
+
+  $rq = new folksoResupQuery();
+  switch ($result_code){
+  case '404':
+    $i->query($rq->resremove($url));
+    header('http/1.1 200 Deleted');
+    print "Removed the resource $url from the system.";
+    return;
+    break;
+  case '200':
+    $i->query($rq->resmodtitle($url, $newtitle));
+
+  }
 
 }
 
