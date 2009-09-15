0bf3a61a2479d8a1f0ade1e6929b315924085f2d
Joseph Fahey <fahey.joseph@gmail.com>
2009-09-15 16:08:00 +0200

    Finalize character replacement table and add insert for equivalences.

--- sql/tables/aux-tables.sql
+++ sql/tables/aux-tables.sql
@@ -1,9 +1,49 @@
 
 
-create table accented_characters(
-       accented char(1) not null primary key, 
-       noaccent char(1) not null,
-       accented_code tinyint unsigned not null, 
-       index coded (accented_code, noaccent),
-       index chared (accented, noaccent))
+create table replace_characters
+       (toreplace_code int unsigned not null primary key, 
+       replacewith char(1) not null,
+       index coded (toreplace_code, replacewith))
+character set utf8
+collate utf8_general_ci
 ENGINE=InnoDB;
+
+
+insert into replace_characters 
+       (replacewith, toreplace_code)
+       values
+       ('a', ord('à')),
+       ('a', ord('ä')),
+       ('a', ord('â')),
+
+       ('c', ord('ç')),
+
+       ('e', ord('é')),
+       ('e', ord('è')),
+       ('e', ord('ë')),
+       ('e', ord('ê')),
+
+       ('i', ord('î')),
+       ('i', ord('ï')),
+
+       ('o', ord('ô')),
+       ('o', ord('ö')),
+       
+       ('u', ord('û')),
+       ('u', ord('ü')),
+       ('u', ord('ù')),
+
+       ('y', ord('ŷ')),
+       ('y', ord('ÿ')),
+
+       ('a', ord('æ')),
+
+       ('-', ord(' ')),
+       ('-', ord('_')),
+       ('-', ord(',')),
+       ('-', ord(';')),
+       ('-', ord('.')),
+       ('-', ord(':')),
+       ('-', ord('~'));
+
+
