29afa5519fd5b5a5c819413cbcbde0e33b0385c8
Joseph Fahey <fahey.joseph@gmail.com>
2009-09-17 03:31:21 +0200

    Starting work on new folksoResponse. some basic functions setup. + tests

---
+++ php/folksoResponse.php
@@ -0,0 +1,159 @@
+<?php
+
+/**
+ * @package Folkso
+ * @author Joseph Fahey
+ * @copyright 2009 Gnu Public Licence (GPL)
+ * @subpackage Tagserv
+ */
+
+/**
+ * @package Folkso
+ * 
+ */
+require_once('folksoTags.php');
+
+class folksoResponse {
+
+  public $status;
+  public $statusMessage;
+  private $errorDeclared;
+  private $body;
+  public $error_body;
+
+  private $httpStatus = array(200 => '0K', 
+                              201 => 'Created',
+                              202 => 'Accepted',
+                              203 => 'Non-Authoritative Information',
+                              204 => 'No Content',
+                              205 => 'Reset content',
+                              304 => 'Not Modified',
+                              400 => 'Bad Request',
+                              401 => 'Unauthorized',
+                              403 => 'Forbidden',
+                              404 => 'Not Found',
+                              405 => 'Method Not Allowed',
+                              406 => 'Not Acceptable',
+                              500 => 'Internal Server Error',
+                              501 => 'Not Implemented'
+                              );
+
+  /**
+   * Internal representation of output content type. One of 'xml',
+   * 'text', 'html'...
+   */
+  private $outType;
+
+  function __construct() {
+    $this->errorDeclared = false;
+
+  }
+
+  /**
+   * @return String The body to be returned.
+   */
+  public function body() {
+    return $this->body;
+  }
+
+  /**
+   * Append $str to response body, inserting a newline between
+   * successive calls. (This mimics using php's print function when
+   * outputting directly.)
+   *
+   * @param $str String
+   */
+  public function t ($str) {
+    if ($this->body) {
+      $this->body = $this->body . "\n" . $str;
+    }
+    else {
+      $this->body = $str;
+    }
+  }
+
+  /**
+   * Declare an error. Sets internal error status to 'TRUE'.
+   *
+   * @param $status Integer HTTP Error code
+   * @param $message String (optional) Error message
+   */
+  public function setError($status, $message = null) {
+    if (! isset($this->httpStatus[$status])){
+      trigger_error("Invalid HTTP status: $status",
+                    e_user_warning);
+    }
+    $this->errorDeclared = true;
+    $this->status = $status;
+    $this->statusMessage 
+      = $message ? $message : $this->httpStatus[$status];
+  }
+
+  public function dbError($status = 500, $message = null){
+    $this->setError($status, $message ? $message : 'Database error');
+  }
+
+  public function setOk ($status, $message = null){
+    if (! isset($this->httpStatus[$status])){
+      trigger_error("Invalid HTTP status: $status",
+                    e_user_warning);
+    }
+    $this->errorDeclared = false;
+    $this->status = $status;
+    $this->statusMessage = 
+      $message ? $message : $this->httpStatus[$status];
+  }
+
+  /**
+   * Translating true to true or false to false. Hmmm
+   */
+  public function isError() {
+    if ($this->errorDeclared) {
+      return true;
+    }
+  }
+
+  public function setType($str) {
+    if (! in_array($str, array('xml', 'text', 'html'))){
+      trigger_error("Invalid output content type", e_user_warning);
+      $str = 'text';
+    }
+    $this->outType = $str;
+  }
+
+  /**
+   * Returns the string for the HTTP content-type header but does not
+   * set the header.
+   *
+   * @return String
+   */  
+  public function contentType () {
+    $type = '';
+    switch ($this->outType) {
+    case 'xml': 
+      $type =  'text/xml';
+      break;
+    case 'text':
+      $type =  'text/text';
+      break;
+    case 'html':
+      $type =  'text/html';
+      break;
+    default: 
+      $type =  'text/text';
+  }
+    return 'Content-Type: ' . $type;
+  }
+
+
+  /**
+   * In case of an error, we make sure that the old body is not
+   * output.
+   * 
+   * @param $str String
+   */
+  public function errorBody($str) {
+    $this->debug_body = $this->body;
+    $this->body = $str;
+  }
+}
--- tests/folksoResponse-test.php
+++ tests/folksoResponse-test.php
@@ -2,42 +2,67 @@
 require_once('unit_tester.php');
 require_once('reporter.php');
 include('folksoTags.php');
-//include('/usr/local/www/apache22/lib/jf/fk/folksoResponse.php');
-//include('/usr/local/www/apache22/lib/jf/fk/folksoQuery.php');
 
 
 class testOffolksoResponse extends  UnitTestCase {
 
-  public $rep;
-  public $query;
-  public $dbc;
-  public $wsse;
-
-    function testConstructor () {
-      
-      $this->query = new folksoQuery(array('REQUEST_METHOD' => 'GET',
-                                           'REMOTE_ADDR' => '127.0.0.1',
-                                           'REMOTE_HOST' => 'localhost'),
-                                     array('folksouri' => 'example.com'),
-                                     array());
 
 
-      $this->dbc = new folksoDBconnect('yo', 'yo', 'yo', 'yo');
-      $this->wsse = new folksoWsseCreds('yo');
 
-      $test_f = create_function('', 'return true;');
-      $act_f = create_function('', 'print "<p>Action!</p>"; return "ok";');
-      $this->rep = new folksoResponse('get', 
-                                      array('required' => array('uri')),
-                                      $act_f);
-      
-      $this->assertTrue(($this->rep instanceof folksoResponse));
-      $this->assertTrue($this->rep->activatep($this->query, $this->wsse)); 
-      $this->assertEqual($this->rep->Respond($this->query, $this->wsse, $this->dbc), "ok");
+  function testBasic () {
+    $r = new folksoResponse();
+    $this->assertIsA($r, folksoResponse);
 
-    }
-}//end class
 
+  }
+  function testBodyFuncs () {
+    $r = new folksoResponse();
+    $r->t('Hello');
+    $this->assertTrue(is_string($r->body()));
+    $this->assertEqual($r->body(), 'Hello', 
+                       "Not returning body() correctly");
+    $r->t('there');
+    $this->assertEqual($r->body(), "Hello\nthere", 
+                       "Newline insertion fail");
+
+  }
+
+  function testErrorFuncs () {
+    $r = new folksoResponse();
+    $r->setError(404);
+    $this->assertEqual($r->status, 404, "Error status problem.");
+    $this->assertEqual($r->statusMessage,
+                       'Not Found',
+                       "Default error status message not working.");
+
+    $r2 = new folksoResponse();
+    $r2->setError(404, "something is wrong");
+    $this->assertEqual($r2->statusMessage, "something is wrong",
+                       "Problem setting error status message");
+    $r3 = new folksoResponse();
+    $r3->setError(404);
+    $this->assertEqual($r3->statusMessage, "Not Found",
+                       "Problem setting default 404 error status message:". $r3->statusMessage.":");
+
+  }
+
+  function testContentType () {
+    $r = new folksoResponse();
+    $r->setType('xml');
+    $this->assertEqual($r->contentType(), 'Content-Type: text/xml', 
+                       'XML content type not correctly determined');
+    $r2 = new folksoResponse();
+    $this->assertEqual($r2->contentType(),
+                       'Content-Type: text/text',
+                       'Default content type not correctly selected');
+    $r3 = new folksoResponse();
+    $r3->setType('html');
+    $this->assertEqual($r3->contentType(),
+                       'Content-Type: text/html',
+                       'HTML content type not correctly determined');
+  }
+
+}
 
 $test = &new testOffolksoResponse();
 $test->run(new HtmlReporter());
