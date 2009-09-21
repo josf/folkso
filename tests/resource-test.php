a4ff9c5be992ecfe4ecd18c23f019b79780cb36d
Joseph Fahey <fahey.joseph@gmail.com>
2009-09-21 17:56:59 +0200

    add tests for getTagsIds

--- tests/resource-test.php
+++ tests/resource-test.php
@@ -16,7 +16,7 @@ class testOfResource extends  UnitTestCase {
                                      'testy', 'testostonomie');
   }
 
-   function testGet () {
+   function testGetTagsIds_text () {
      $q = new folksoQuery(array(), 
                           array('folksores' => 'http://example.com/1'),
                           array());
@@ -24,9 +24,34 @@ class testOfResource extends  UnitTestCase {
      $r = getTagsIds($q, $cred, $this->dbc);
      $this->assertIsA($r, folksoResponse, 
                 'getTagsIds() does not return a folksoResponse object');
-     print $r->body();
+     $this->assertEqual($r->status, 200, 
+                        'getTagsIds returns incorrect status');
+     $this->assertTrue(strlen($r->body()) > 1, 
+                       'Not returning any body');
+     $this->assertPattern('/tagone/',
+                          $r->body(),
+                          '"tagone" tag not found in response body');
+   }
+
+   function testGetTagsIds_xml () {
+     $q = new folksoQuery(array(), 
+                          array('folksores' => 'http://example.com/1',
+                                'folksodatatype' => 'text/xml'),
+                          array());
+     $cred = new folksoWsseCreds('zork');
+     $r = getTagsIds($q, $cred, $this->dbc);
+     $this->assertEqual($r->status, 200,
+                        'getTagsIds (xml) returns error status');
+     
+     $this->assertPattern('/<taglist/',
+                          $r->body(),
+                          'Does not look like xml'
+                          );
+     
 
    }
+
+
 }//end class
 
 $test = &new testOfResource();
