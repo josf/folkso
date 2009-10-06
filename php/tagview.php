0b8ef569ecb59a00241192c401ebfa1139e2555f
Joseph Fahey <fahey.joseph@gmail.com>
2009-10-06 15:30:55 +0200

    Div with bloc_orange id for related tags

--- php/tagview.php
+++ php/tagview.php
@@ -166,7 +166,9 @@ if ($page->tr->is_valid()) {
   $reltags = $page->RelatedTags();
   if ($reltags) {
      print "<h2>Tags voisins : </h2>";
+print '<div id="bloc_orange">';
      print $reltags;
+print "</div>";
   }
   print $taglist; 
 }
