a64e73ad283e1c7b10c0f264141e92a7e43b278a
Joseph Fahey <fahey.joseph@gmail.com>
2009-10-22 15:46:27 +0200

    started utilisateurs.php

---
+++ php/utilisateurs.php
@@ -0,0 +1,15 @@
+<?php
+
+
+  /**
+   * Web interface for user related folkso activities.
+   *
+   * @package Folkso
+   * @author Joseph Fahey
+   * @copyright 2009 Gnu Public Licence (GPL)
+   * @subpackage Tagserv
+   */
+
+require_once('folksoTags.php');
+
+
