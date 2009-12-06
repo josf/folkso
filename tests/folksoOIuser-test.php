56f2f7284c275aa9a209ba0feb4298b3696cdd3d
Joseph Fahey <fahey.joseph@gmail.com>
2009-12-06 15:00:41 +0100

    Many more tests for OIuser.

--- tests/folksoOIuser-test.php
+++ tests/folksoOIuser-test.php
@@ -30,10 +30,22 @@ class testOffolksoOIuser extends  UnitTestCase {
                            'simple but valid url fails oid check');
          $this->assertFalse($oi->validateLoginId('bob.kom'),
                             'invalid oid url should fail');
-         $this->assertTrue($oi->validateLoginId('http://myspace/gustav'),
+         $this->assertTrue($oi->validateLoginId('http://myspace.com/gustav'),
                            'gustav url should validate, but is not');
-         $this->assertTrue($oi->exists('http://myspace/gustav'),
+         $this->assertTrue($oi->exists('http://myspace.com/gustav'),
                            'not existing gustav');
+
+         $gus = new folksoOIuser($this->dbc);
+         $gus->userFromLogin('http://myspace.com/gustav');
+         $this->assertEqual($gus->nick, 'gustav',
+                            'Not retreiving correct nick');
+         $this->assertTrue(strlen($gus->nick) > 2,
+                           'nick is too short or does not exist');
+         $this->assertEqual($gus->firstName, 'Gustave',
+                            'Not retrieving correct first name');
+         $this->assertEqual($gus->email, 'gflaub@sentimental.edu',
+                            'Not retrieving correct email address');
+         $this->assertTrue($gus->Writeable(), 'retrieved user is not writeable');
    }
 }//end class
 
