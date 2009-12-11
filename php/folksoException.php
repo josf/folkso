b4262cabff88155a4faa6269315c3f8d6ef27c15
Joseph Fahey <fahey.joseph@gmail.com>
2009-12-11 11:49:06 +0100

    Add custom Exception class.

---
+++ php/folksoException.php
@@ -0,0 +1,16 @@
+<?php
+  /**
+   * 
+   *
+   * @package Folkso
+   * @subpackage
+   * @author Joseph Fahey
+   * @copyright 2009 Gnu Public Licence (GPL)
+   */
+
+  /**
+   * @package Folkso
+   */
+class badSidException extends Exception { }
+class insufficientDataException extends Exception { }
+
