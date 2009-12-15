c5f14856b068a4398efbc9153d6dbd0c7e75dcb8
Joseph Fahey <fahey.joseph@gmail.com>
2009-12-15 15:42:27 +0100

    update of tag-event.sql for new userids. new indexes to for users.

--- sql/tables/tag-event.sql
+++ sql/tables/tag-event.sql
@@ -4,13 +4,16 @@ CREATE TABLE tagevent
        tag_id INT UNSIGNED NOT NULL,
        resource_id INT UNSIGNED NOT NULL, 
        meta_id INT UNSIGNED DEFAULT 1 NOT NULL,
-       user_id INT UNSIGNED NOT NULL,
+       userid varchar(255) NOT NULL,
        tagtime TIMESTAMP NOT NULL,
        INDEX tagdex (tag_id, resource_id),
        INDEX resdex (resource_id, tag_id),
+       INDEX useres (userid, resource_id),
+       INDEX usetag (userid, tag_id),
        FOREIGN KEY (tag_id) REFERENCES tag (id),
        FOREIGN KEY (resource_id) REFERENCES resource (id),
-       FOREIGN KEY (meta_id) REFERENCES metatag (id))
+       FOREIGN KEY (meta_id) REFERENCES metatag (id),
+       FOREIGN KEY (userid) REFERENCES users (userid))
     ENGINE=InnoDB;
 
 -- (local-set-key [(control c) (b)] 'sql-snip)
