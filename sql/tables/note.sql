d1bca129cc0a059d29a8e401c946864741d13b3d
Joseph Fahey <fahey.joseph@gmail.com>
2009-12-15 19:03:06 +0100

    update note table schema for userids

--- sql/tables/note.sql
+++ sql/tables/note.sql
@@ -3,8 +3,11 @@ CREATE TABLE note
        (id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
        resource_id INT UNSIGNED NOT NULL,
        note TEXT NOT NULL,
-       user_id INT UNSIGNED NOT NULL DEFAULT 9999,
+       userid varchar(255) not null,
        created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-       INDEX resid (resource_id))
+       FOREIGN KEY (userid) REFERENCES users (userid),
+       FOREIGN KEY (resource_id) REFERENCES resource (id),
+       INDEX resid (resource_id),
+       INDEX useres (userid, resource_id))
        ENGINE = InnoDB;
        
