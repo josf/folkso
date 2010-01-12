a5a2edfd0728400b831c59c83c20fcddcbf2127c
Joseph Fahey <fahey.joseph@gmail.com>
2010-01-12 18:06:10 +0100

    started folksonomie.js

---
+++ js/folksonomie.js
@@ -0,0 +1,18 @@
+/* 
+ * Copyright (c) 2010 Joseph Fahey 
+ * GNU Public Licence.
+ * 
+ * Common lib for folksonomie related javascript
+ * 
+ * These functions should eventually be used by all the folksonomie js
+ *  as a common way for setting up interactive pages.
+ * 
+ */
+
+(function() {
+     var fK = window.fK = function(config) {
+
+     };
+     
+
+})();
