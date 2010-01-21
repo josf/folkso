3608fdc8b32c6d491f0bb3a3daed16a52c224a95
Joseph Fahey <fahey.joseph@gmail.com>
2010-01-21 11:22:22 +0100

    started myfabula.js

---
+++ js/myfabula.js
@@ -0,0 +1,25 @@
+/* 
+ * Copyright (c) 2010 Joseph Fahey 
+ * GNU Public Licence.
+ * 
+ * JS specific to the myfabula.php page.
+ * 
+ * Requires jQuery and folksonomie.js
+ * 
+ * 
+ * 
+ */
+
+$(document).ready(function()
+                  {
+
+
+                  
+
+
+
+
+
+
+
+                  }); // end document.ready
