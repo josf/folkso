ed10bac88081c5b10027ccde61a151c869ed3c93
Joseph Fahey <fahey.joseph@gmail.com>
2010-02-07 22:17:25 +0100

    Use $loc->javascript_path in script elements

--- php/web/samplepage.php
+++ php/web/samplepage.php
@@ -13,10 +13,14 @@ $fp = new folksoPage();
 
 <script type="text/javascript"
     src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.js"></script> 
-<script type="text/javascript" src="js/jquery.autocomplete.js"> </script>
-<script type="text/javascript" src="jquery.jqote.js"></script>
-<script type="text/javascript" src="folksonomie.js"></script>
-<script type="text/javascript" src="faboid.js"></script>
+<script type="text/javascript" 
+  src="<?php print $fp->javascript_path() ?>jquery.autocomplete.js"> </script>
+<script type="text/javascript" 
+  src="<?php print $fp->javascript_path() ?>jquery.jqote.js"></script>
+<script type="text/javascript" 
+  src="<?php print $fp->javascript_path() ?>folksonomie.js"></script>
+<script type="text/javascript" 
+  src="<?php print $fp->javascript_path() ?>faboid.js"></script>
 
 <?php // outputs a <script> elememnt
   print $fp->jsHolder($fp->fKjsLoginState('fK.loginState'));
