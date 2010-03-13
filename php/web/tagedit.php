a9a825d9486d24b5f11ac20e31a01920bea960cd
Joseph Fahey <fahey.joseph@gmail.com>
2010-03-13 22:28:05 +0100

    * php/web/tagedit.php: Using google api jquery 1.4.2

--- php/web/tagedit.php
+++ php/web/tagedit.php
@@ -21,7 +21,8 @@
 </link>
   
 
-<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" 
+        src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js"></script>
 <script type="text/javascript" src="js/jquery.autocomplete.js">
   </script>
 
