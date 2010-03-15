33984f692411882a958f2d8059dbf1da2228cbe1
Joseph Fahey <fahey.joseph@gmail.com>
2010-03-15 22:34:28 +0100

    * php/folksovisit.php: Using new parameter in fkSErver to allow   anonymous posting

---
+++ php/folksovisit.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ *
+ * @package Folkso
+ * @author Joseph Fahey
+ * @copyright 2010 Gnu Public Licence (GPL)
+ * @subpackage Tagserv
+ */
+
+require_once "folksoServer.php";
+require_once "folksoResource.php";
+require_once "folksoUrlRewriteResource.php";
+
+/**
+ * Interface for adding new resources to the database.
+ */
+
+
+$srv = new folksoServer(array('methods' => array('POST'),
+                              'access_mode' => 'LOCAL',
+                              'rewrite' => new folksoUrlRewriteResource(),
+                              'allow_anonymous_post' => true));
+
+$srv->addResponseObj(new folksoResponder('post',
+                                         array('required' => array('resource')),
+                                         'visitPage'));
+
+$srv->Respond();
+                                               
