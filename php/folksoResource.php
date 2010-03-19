88c68e6dc2c4d0125514053acbb2942973c0e371
Joseph Fahey <fahey.joseph@gmail.com>
2010-03-19 22:06:28 +0100

    * php/folksoResource.php: typo

--- php/folksoResource.php
+++ php/folksoResource.php
@@ -239,8 +239,8 @@ function tagCloudLocalPop (folksoQuery $q, folksoDBconnect $dbc, folksoSession $
       $r->t($dd->line($row->cloudweight, 
                       "resourceview.php?tagthing=".
                       $row->tagid, 
-                      htmlspecialchars($row->tagdisplay, ENT_NOQUOTES, 'UTF-8'),
-                      )."\n");
+                      htmlspecialchars($row->tagdisplay, ENT_NOQUOTES, 'UTF-8')
+                      ) . "\n");
     }
     $r->t($dd->endform());
     return $r;
