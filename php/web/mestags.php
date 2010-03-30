cd8be238ef7f2f780cc42a62831d1d743e5e60ec
Joseph Fahey <fahey.joseph@gmail.com>
2010-03-30 15:11:31 +0200

    * php/web/mestags.php: Starting out

---
+++ php/web/mestags.php
@@ -0,0 +1,44 @@
+<?php
+  /**
+   * @package Folkso
+   * @author Joseph Fahey
+   * @copyright 2010 Gnu Public Licence (GPL)
+   * @subpackage webinterface
+   */
+
+require_once('folksoDBconnect.php');
+require_once('folksoDBinteract.php');
+require_once('folksoFabula.php');
+require_once('folksoClient.php');
+
+
+require("/var/www/dom/fabula/commun3/head_libs.php");
+require("/var/www/dom/fabula/commun3/head_folkso.php");
+require("/var/www/dom/fabula/commun3/head_dtd.php");
+
+print "<html>\n<head>";
+
+require("/var/www/dom/fabula/commun3/head_meta.php");
+require("/var/www/dom/fabula/commun3/head_css.php");
+require("/var/www/dom/fabula/commun3/head_javascript_folkso.php");
+
+require ('browser_detect.php');
+if (stristr($_SERVER['HTTP_USER_AGENT'], 'iPhone')) {
+echo ("</head>\n<body>");
+echo ("<h1 class=\"titre_iphone\">Visitez notre site optimis<C3><A9> <br><a href=\"http://iphone.fabula.org\">iphone.fabula.org</a></h1>");
+} else {
+if ( (browser_detection( 'os' )== "mac" ) && (browser_detection( 'browser' ) =="moz") ) {
+echo "<style>\n#tabs-menu {\nheight: 17px;\n}\n</style>";
+}
+echo ("</head>\n<body>");
+}
+
+
+require("/var/www/dom/fabula/commun3/html_start.php");
+
+/** your stuff here **/
+
+
+
+
+include("/var/www/dom/fabula/commun3/foot.php");
