8e837a6d1e1af2da59b21d5c4266243c28f8fe0e
Joseph Fahey <fahey.joseph@gmail.com>
2010-07-06 23:10:38 +0200

    * php/user.php: should have been added long ago.
    Just put getFavoriteTags responder in.

---
+++ php/user.php
@@ -0,0 +1,68 @@
+<?php
+
+
+  /**
+   * Web service for accessing users tags.
+   *
+   * @package Folkso
+   * @author Joseph Fahey
+   * @copyright 2009-2010 Gnu Public Licence (GPL)
+   * @subpackage Tagserv
+   */
+
+require_once('folksoUserServ.php');
+
+$srv = new folksoServer(array( 'methods' => array('POST', 'GET', 'HEAD', 'DELETE'),
+                               'access_mode' => 'ALL'));
+
+$srv->addResponseObj(new folksoResponder('get',
+                                         array('required' => array('mytags')),
+                                         'getMyTags'));
+$srv->addResponseObj(new folksoResponder('get',
+                                         array('required' => array('tag')),
+                                         'getUserResByTag'));
+
+$srv->addResponseObj(new folksoResponder('get',
+                                         array('required' => array('check', 'fbuid')),
+                                         'checkFBuserId'));
+$srv->addResponseObj(new folksoResponder('get',
+                                         array('required' =>
+                                               array('fblogin')),
+                                         'loginFBuser'));
+$srv->addResponseObj(new folksoResponder('get',
+                                         array('required' => array('subscribed'),
+                                               'exclude' => array('fblogin', 'check', 'tag')),
+                                         'userSubscriptions'));
+
+$srv->addResponseObj(new folksoResponder('get',
+                                         array('required' => array('userdata')),
+                                         'getUserData'));
+
+$srv->addResponseObj(new folksoResponder('get',
+                                         array('required' => array('recent')),
+                                         'recentlyTagged'));
+
+$srv->addResponseObj(new folksoResponder('get',
+                                         array('required' => array('favorites')),
+                                         'getFavoriteTags'));
+
+                                               
+$srv->addResponseObj(new folksoResponder('post',
+                                         array('required' => array('addsubscription', 'tag')),
+                                         'addSubscription'));
+     
+$srv->addResponseObj(new folksoResponder('post',
+                                         array('required' => array('rmsub', 'tag')),
+                                         'removeSubscription'));
+$srv->addResponseObj(new folksoResponder('post',
+                                         array('required' => array('setfirstname',
+                                                                   'setlastname'),
+                                               'exclude' => array('rmsub', 'tag')),
+                                         'storeUserData'));
+                                               
+$srv->Respond();
+  
+
+
+
+
