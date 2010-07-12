f0edbec169bba5d33b4cca68a70db32c6d79e89e
Joseph Fahey <fahey.joseph@gmail.com>
2010-07-12 16:27:59 +0200

    * php/web/moi.php: More or less working. No cv yet.

--- php/web/moi.php
+++ php/web/moi.php
@@ -15,13 +15,85 @@ require_once 'folksoDBinteract.php';
 require_once 'folksoFabula.php';
 require_once 'folksoUserServ.php';
 require_once 'folksoPage.php';
+require_once 'folksoUserServ.php';
+require_once 'folksoInteract.php';
 
 if ((! $fp) || (! $fp instanceof folksoPage)) {
   $fp = new folksoPage();
 } 
 
+if ((! $loc) || (! $loc instanceof folksoFabula)) {
+  $loc = new folksoFabula();
+}
+$dbc = $loc->locDBC();
+$u = new folksoUser($dbc);
 
+$rawUser = strip_tags($_GET['user']);
+if (strlen($rawUser) > 255) {
+  $rawUser = null;
+}
 
+if ((! $rawUser) || 
+    (! $u->validateUid($rawUser)) ||
+    (! $u->userFromUserId($rawUser))) {
+  // userFromUserId returns false when user is not found
+  header('HTTP/1.1 404 User not found');
+  print "The user id you supplied is either invalid or does not correspond to a real user";
+  exit();
+}
+
+#ifdef DEBUG
+$debug = '';
+#endif
+
+$fkint = new folksoOnServer($dbc);
+$userDataReq = $fkint->userDataReq($u);
+$userFaveReq = $fkint->userFavoriteTags($u);
+
+if (! ($userDataReq->status  == 200)) {
+  $noUserData = true;
+
+#ifdef DEBUG
+  $debug .= '<p>bad status on userDataReq: ' . $userDataReq . '</p>';
+#endif
+}
+else {
+  $userData_DOM = new DOMDocument();
+  if (! $userData_DOM->loadXML($userDataReq->body())) {
+    $noUserData = true;
+#ifdef DEBUG
+    $debug .= '<p>userData failed to load as xml</p>';
+#endif
+  }
+  else {  // process and build $userDataHtml which will be used in the page
+    $udataXsl = new DOMDocument();
+    $udataXsl->load($loc->xsl_dir . "userdata_display.xsl");
+    $udataProc = new XsltProcessor();
+    $udataXsl = $udataProc->importStylesheet($udataXsl);
+    $udataTrans = $udataProc->transformToDoc($userData_DOM);
+    $userDataHtml = $udataTrans->saveXML();
+
+  }    
+
+  if (($userFaveReq->status == 200) &&
+      (strlen($userFaveReq->body()) > 0)) {
+    $userFave_DOM = new DOMDocument();
+    $userFave_DOM->loadXML($userFaveReq->body());
+
+    $ufaveXsl = new DOMDocument();
+    $ufaveXsl->load($loc->xsl_dir . "userfaves_display.xsl");
+    $ufaveProc = new XsltProcessor();
+    $udataXsl = $ufaveProc->importStylesheet($ufaveXsl);
+    $ufaveTrans = $ufaveProc->transformToDoc($userFave_DOM);
+    $userFaveHtml = $ufaveTrans->saveXML();
+  }
+#ifdef DEBUG
+  else {
+    $debug .= '<p>No favorites found: userFaveReq->status: ' . $userFaveReq->status 
+      . ' bodylength: ' . strlen($userFaveReq->body()) . '</p>';
+  }
+#endif 
+}
 
 require("/var/www/dom/fabula/commun3/head_libs.php");
 require("/var/www/dom/fabula/commun3/head_folkso.php");
@@ -49,3 +121,26 @@ require("/var/www/dom/fabula/commun3/html_start.php");
 ?> 
 <div id="colonnes_nouvelles">
 <div id="colonnes-un">
+
+<?php
+#ifdef DEBUG
+  print $debug;
+#endif 
+
+  if ($noUserData ||
+      (strlen($userDataHtml) == 0)) {
+    print "<p>Les informations concernant cet utilisateur ne sont pas disponibles actuellement.</p>";
+  }
+  else {
+    print $userDataHtml;
+    
+    if (strlen($userFaveHtml) > 0) {
+      print $userFaveHtml;
+    }
+  }
+?>
+</div></div>
+<?php
+
+include("/var/www/dom/fabula/commun3/foot.php");
+
