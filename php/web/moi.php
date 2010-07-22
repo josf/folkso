24ecee35d78f03f5a27708c825f2c4b984a6018c
Joseph Fahey <fahey.joseph@gmail.com>
2010-07-22 08:12:46 +0200

    * php/web/moi.php: Minor reformat

--- php/web/moi.php
+++ php/web/moi.php
@@ -50,12 +50,10 @@ if ($rawUser) {
 }
 // otherwise we might be getting the name as parameters
 elseif ($_GET['first'] && $_GET['last']) {
-  $first = preg_replace('/[^a-zA-Z ]/',
-                        '',
-                        $_GET['first']);
-  $last = preg_replace('/[^a-zA-Z ]/',
-                       '',
-                       $_GET['last']);
+  // letters only in names
+  $first = preg_replace('/[^a-zA-Z]/', '', $_GET['first']);
+  $last = preg_replace('/[^a-zA-Z]/', '', $_GET['last']);
+
   if ((strlen($first) > 1) &&
       (strlen($last) > 1)) {
     if (! $u->userFromName($first, $last)){
