22736d108e05b3584d865ea904ed6ee245db0502
Joseph Fahey <fahey.joseph@gmail.com>
2010-08-23 18:26:54 +0200

    * php/folksoSession.php: Remove error triggering. Just count on exceptions

--- php/folksoSession.php
+++ php/folksoSession.php
@@ -184,19 +184,13 @@ class folksoSession {
 
 /**
  * @param $session_id (optional)
+ * @throws dbException
  */
  public function killSession ($session_id = null) {
    $sid = $session_id ? $session_id : $this->sessionId;
    if ($this->validateSid($sid)){
-   $i = new folksoDBinteract($this->dbc);
-   if ($i->db_error()){
-     trigger_error("Database connection error: " . $i->error_info(),
-                   E_USER_ERROR);
-   }
-   $i->query( "delete from sessions where token = '" . $i->dbescape($sid) . "'");
-   }
-   else {
-     trigger_error('invalid session id', E_USER_WARNING);
+     $i = new folksoDBinteract($this->dbc);
+     $i->query( "delete from sessions where token = '" . $i->dbescape($sid) . "'");
    }
  }
 
