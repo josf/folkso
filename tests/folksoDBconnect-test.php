e90a8496c1697871231ee515a95a5bc4aea02cd1
Joseph Fahey <fahey.joseph@gmail.com>
2010-12-21 22:40:24 +0100

    add basic mysqli connection test, cleanup, add quotes to assertIsA

--- tests/folksoDBconnect-test.php
+++ tests/folksoDBconnect-test.php
@@ -8,18 +8,19 @@ require_once('folksoDBconnect.php');
 
 class testOffolksoDBConnect extends  UnitTestCase {
 
-  function testConnection () {
-    $con = new folksoDBconnect('localhost','root','hellyes','folksonomie');
-    $this->assertTrue($con instanceof folksoDBconnect);
-    $this->assertTrue($con->db_obj() instanceof mysqli);
+  function testBasic () {
+    $m = new mysqli('localhost', 'tester_dude', 'testy', 'testostonomie');
+    $this->assertIsA($m, 'mysqli', "mysqli initialization failing");
+  }
 
+  function testConnection () {
     $con2 = new folksoDBconnect('localhost', 'tester_dude',
                                 'testy', 'testostonomie');
 
-    $this->assertIsA($con2, folksoDBconnect,
+    $this->assertIsA($con2, 'folksoDBconnect',
                      'problem with object creation');
     $this->assertIsA($con2->db_obj(),
-                     mysqli,
+                     'mysqli',
                      'Not creating mysqli object with db_obj()');
 
   }
