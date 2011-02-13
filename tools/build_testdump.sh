f19f440e437793971f48559e11adad99f22e6904
Joseph Fahey <fahey.joseph@gmail.com>
2011-02-13 12:15:58 +0100

    --skip-triggers option for building DB
    Otherwise mysql complains about adding new triggers.

--- tools/build_testdump.sh
+++ tools/build_testdump.sh
@@ -2,6 +2,6 @@
 DUMPDIR=/home/joseph/sect/fab
 TOOLDIR=/home/joseph/sect/fab/tools/
 
-mysqldump -u root --password=hellyes --no-create-db --no-create-info testostonomie > $TOOLDIR/testdump-raw.sql
+mysqldump -u root --password=hellyes --no-create-db --no-create-info --skip-triggers testostonomie > $TOOLDIR/testdump-raw.sql
 
 cat $TOOLDIR/testdump_init.sql  $TOOLDIR/testdump-raw.sql  > $DUMPDIR/testdump_test.sql
